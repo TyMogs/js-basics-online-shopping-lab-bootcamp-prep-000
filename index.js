776e899501fa0f4a24682fe9d040a23c6a64d28e
Tyler Morris <tyler.a.morris72@gmail.com>
2018-01-16 17:05:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -66,6 +66,8 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if(!cardNumber) {
-    return 
+    return "Sorry, we don't have a credit card on file for you."
+  } else {
+      
   }
 }
