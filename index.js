6cbf48e47ec097d1d2f33a9cd99a5942d714eb80
Tyler Morris <tyler.a.morris72@gmail.com>
2018-01-16 17:08:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -70,4 +70,8 @@ function placeOrder(cardNumber) {
   } else {
       console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
   }
+  for(var i = 0; i < cart.length; i++) {
+    cart.pop(cart[i])
+  }
+  return cart
 }
